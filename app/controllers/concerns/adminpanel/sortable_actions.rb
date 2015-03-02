a228a8a761721e9af819a567543a75f3f92aa78e
vicocamacho <vicocamacho88@gmail.com>
2015-03-02 13:52:50 -0600

    Add ability to use friendly id when defined

--- app/controllers/concerns/adminpanel/sortable_actions.rb
+++ app/controllers/concerns/adminpanel/sortable_actions.rb
@@ -1,21 +1,33 @@
 module Adminpanel
   module SortableActions
     extend ActiveSupport::Concern
+    included do
+      before_action :set_resource, only: [
+        :move_to_better,
+        :move_to_worst
+      ]
+    end
 
     def move_to_better
-      resource = @model.find(params[:id])
       resource.move_to_better_position
       update_index_table
 
     end
 
     def move_to_worst
-      resource = @model.find(params[:id])
       resource.move_to_worst_position
       update_index_table
     end
 
   protected
+    def set_resource
+      if @model.respond_to? :friendly
+        @resource_instance = @model.friendly.find(params[:id])
+      else
+        @resource_instance = @model.find(params[:id])
+      end
+    end
+
     def update_index_table
       respond_to do |format|
         format.js do
